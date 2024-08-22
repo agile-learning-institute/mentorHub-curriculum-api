4fc13463013178cb21e67ff7dc51a434f6b8b1fb
Mike Storey <mike@agile-learning.institute>
2024-08-22 15:47:37 -0400

    service layer updates

--- src/services/curriculum_services.py
+++ src/services/curriculum_services.py
@@ -5,7 +5,7 @@ logger = logging.getLogger(__name__)
 class CurriculumService:
 
     @staticmethod 
-    def check_user_access():
+    def _check_user_access():
         # Role Based Access logic
         # if user.roles contains Admin, Mentor or
         # if user.roles contains Member and id = user_id
@@ -15,7 +15,7 @@ class CurriculumService:
         # raise Exception("Access Denied")
 
     @staticmethod
-    def encode_resource_data(resource_data):
+    def _encode_resource_data(resource_data):
         # Example encoding logic (this could be any transformation you need)
         # Encode ID
         # Encode Breadcrumb
@@ -24,7 +24,7 @@ class CurriculumService:
 
     @staticmethod
     def get_or_create_curriculum(curriculum_id):
-        CurriculumService.check_user_access()
+        CurriculumService._check_user_access()
 
         mongo_io = MongoIO()
         curriculum = mongo_io.get_curriculum(curriculum_id)
@@ -34,23 +34,23 @@ class CurriculumService:
 
     @staticmethod
     def add_resource_to_curriculum(curriculum_id, resource_data):
-        CurriculumService.check_user_access()
+        CurriculumService._check_user_access()
 
-        resource_data = CurriculumService.encode_resource_data(resource_data)
+        resource_data = CurriculumService._encode_resource_data(resource_data)
         mongo_io = MongoIO()
         return mongo_io.add_resource_to_curriculum(curriculum_id, resource_data)
 
     @staticmethod
     def update_curriculum(curriculum_id, seq, resource_data):
-        CurriculumService.check_user_access()
+        CurriculumService._check_user_access()
 
-        resource_data = CurriculumService.encode_resource_data(resource_data)
+        resource_data = CurriculumService._encode_resource_data(resource_data)
         mongo_io = MongoIO()
         return mongo_io.update_curriculum(curriculum_id, seq, resource_data)
 
     @staticmethod
     def delete_resource_from_curriculum(curriculum_id, seq):
-        CurriculumService.check_user_access()
+        CurriculumService._check_user_access()
 
         mongo_io = MongoIO()
         mongo_io.delete_resource_from_curriculum(curriculum_id, seq)        
