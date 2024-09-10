0bd749dc1d44c0b19572ed4662149941fce48c23
Mike Storey <mike@agile-learning.institute>
2024-09-10 12:30:47 -0400

    improved testing of decoding ObjectId and datetime values

--- test/routes/test_curriculum_routes.py
+++ test/routes/test_curriculum_routes.py
@@ -1,5 +1,7 @@
+from datetime import datetime
 import unittest
 from unittest.mock import patch, MagicMock
+from bson import ObjectId
 from flask import Flask
 from src.routes.curriculum_routes import create_curriculum_routes
 from src.utils.ejson_encoder import MongoJSONEncoder
@@ -8,7 +10,8 @@ class TestCurriculumRoutes(unittest.TestCase):
 
     def setUp(self):
         # Test Data
-        self.sample_curriculum = {"_id":"aaaa00000000000000000001", "name": "A sample document"}
+        self.sample_curriculum_decoded = {"_id":"aaaa00000000000000000001", "date": "2024-07-01 14:30:00", "name": "A sample document"}
+        self.sample_curriculum_encoded = {"_id": ObjectId("aaaa00000000000000000001"), "date": datetime.fromisoformat("2024-07-01T14:30:00"), "name": "A sample document"}
         
         # Set up the Flask test app and register the blueprint
         self.app = Flask(__name__)
@@ -20,14 +23,14 @@ class TestCurriculumRoutes(unittest.TestCase):
     @patch('src.routes.curriculum_routes.CurriculumService.get_or_create_curriculum')
     def test_get_or_create_curriculum_success(self, mock_get_or_create):
         # Mock the CurriculumService's get_or_create_curriculum method
-        mock_get_or_create.return_value = self.sample_curriculum
+        mock_get_or_create.return_value = self.sample_curriculum_encoded
 
         response = self.client.get('/api/curriculum/AAAA00000000000000000001')
         self.assertEqual(response.status_code, 200)
         self.assertTrue(response.is_json)
 
         data = response.get_json()
-        self.assertEqual(data, self.sample_curriculum)
+        self.assertEqual(data, self.sample_curriculum_decoded)
 
     @patch('src.routes.curriculum_routes.CurriculumService.update_curriculum')
     def test_update_curriculum_success(self, mock_update):
